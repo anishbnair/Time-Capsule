0768dc94bf0867f066ac3bfea7e880ae6bd02bef
anishbnair <nbanish@gmail.com>
2018-03-17 12:47:48 -0400

    included model for decades and associated it with user

---
+++ models/decades.js
@@ -0,0 +1,34 @@
+// Model for decades
+
+module.exports = function (sequelize, DataTypes) {
+    var Decades = sequelize.define("Decades", {
+
+        decade: {
+            type: DataTypes.INTEGER,
+        },
+        topic: {
+            type: DataTypes.STRING,
+        },
+        title_tag: {
+            type: DataTypes.STRING,
+        },
+        user_text: {
+            type: DataTypes.STRING,
+        },
+        user_picture: {
+            type: DataTypes.STRING,
+        }
+    });
+
+    Decades.associate = function (models) {
+        // We're saying that a post should belong to a user
+        // A post can't be created without an user due to the foreign key constraint
+        Decades.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Decades;
+};

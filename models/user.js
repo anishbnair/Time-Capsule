1330327786cb8148975bbc05213f5566bd0c443d
anishbnair <nbanish@gmail.com>
2018-03-18 22:29:29 -0400

    associated user model to decades model

--- models/user.js
+++ models/user.js
@@ -23,14 +23,25 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false
     }
   });
+
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
+
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
   User.hook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
+
+  User.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Decades, {
+      onDelete: "cascade"
+    });
+  };
+
   return User;
 };
